f4c5044c712a79db7bf6998abcb0874f70330b64
Yuting PANG <ypangab@connect.ust.hk>
2018-02-22 17:28:33 -0800

    Initial Commit

---
+++ cs174b/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cs174b
+//
+//  Created by Pang Yuting on 2/22/18.
+//  Copyright © 2018 Pang Yuting. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
